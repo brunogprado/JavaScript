e117ef3bb6421d5f843dc3cbc6193daf6ca7133c
brunogprado <brunogprado@me.com>
2023-02-17 12:44:43 -0300

    aula11

---
+++ aula11/ex009.js
@@ -0,0 +1,15 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log('Brasileiro')
+} else {
+    console.log('Estrangeiro!')
+}
+
+let country = 'EUA'
+console.log(`Vivendo em ${country}`)
+if (country != 'Brasil') {
+    console.log('Você é estrangeiro!')
+} else {
+    console.log('Você é brasileiro!')
+}
