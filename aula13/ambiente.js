c02b8dd39aaf719addbce85d53798b28dc0d1772
brunogprado <brunogprado@me.com>
2023-02-24 11:12:14 -0300

    repetições

---
+++ aula13/ambiente.js
@@ -0,0 +1,15 @@
+var c = 1
+while (c <= 6) {
+    console.log(`Tudo bem? ${c}`)
+    c++ // c = c + 1
+}
+
+var d = 1
+do {
+    console.log(`Passo ${d}`)
+    d++ // d = d + 1
+} while (d <=6)
+
+for (var f = 1; f <= 10; f++) {
+    console.log(`teste ${f}`)
+}
---
+++ aula14/ambiente.js
@@ -0,0 +1,6 @@
+console.log('Vai começar...')
+for (var f = 1; f <= 8; f++) {
+    console.log(`teste ${f}`)
+}
+console.log('Terminou!')
+console.log(f)
