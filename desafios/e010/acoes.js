082aa76bc9b27b699c5ea0f8bfc1ad8e31a5b64b
brunogprado <brunogprado@me.com>
2023-03-03 15:36:39 -0300

    9 e 10

---
+++ desafios/e010/acoes.js
@@ -0,0 +1,17 @@
+let saida = document.querySelector('#saida')
+
+function click1() {
+    saida.innerHTML += 'Clicou no primeiro botão.<br>'
+}
+
+function click2() {
+    saida.innerHTML += 'Clicou no segundo botão.<br>'
+}
+
+function click3() {
+    saida.innerHTML += 'Clicou no terceiro botão.<br>'
+}
+
+function click4() {
+    saida.innerHTML += 'Clicou no quarto botão.<br>'
+}
